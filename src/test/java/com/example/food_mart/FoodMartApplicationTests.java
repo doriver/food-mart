b36bca937954f0416f804fac3285fee633ad7866
DESKTOP-I6L47K7\user <leesang@gro>
2026-01-28 18:42:11 +0900

    init project

---
+++ src/test/java/com/example/food_mart/FoodMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_mart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
